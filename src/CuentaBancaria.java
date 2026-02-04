c17f214efb2d38d0d2b2c5b162ab94eca83f30d8
Arant5042 <arantonio.lopez123@gmail.com>
2026-02-04 09:54:37 -0500

    Metodos de la clase CuentaBancaria

--- src/CuentaBancaria.java
+++ src/CuentaBancaria.java
@@ -20,4 +20,32 @@ public class CuentaBancaria
     {
         return saldo;
     }
+
+    public void consignar( int valorAConsignar)
+    {
+        if(valorAConsignar <= 0)
+        {
+            throw new IllegalArgumentException("El valor no es valido");
+        }
+
+        this.saldo += valorAConsignar;
+    }
+
+    public void retirar(int valorARetirar)
+    {
+        if (valorARetirar >= 0 )
+        {
+            throw new IllegalStateException("No se puede retirar más de lo obtenido");
+        }
+
+        if (valorARetirar > saldo)
+        {
+            throw new IllegalStateException("No se puede retirar de más");
+        }
+
+
+        this.saldo -= valorARetirar;
+
+    }
+
 }
