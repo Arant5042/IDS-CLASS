23fd9bd0f3caca42fae8dc69be0a59e7b9749b2a
Arant5042 <arantonio.lopez123@gmail.com>
2026-02-06 09:12:01 -0500

    Envío de mensajes por correo/SMS

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,13 @@ public class Main{
         CuentaBancaria cuenta1 = new CuentaBancaria ("1", 100);
         cuenta1.retirar( 100);
         cuenta1.consignar(200);
-        System.out.printf("El saldo de la cuenta es:" + cuenta1.getSaldo());
+        System.out.println("El saldo de la cuenta es:" + cuenta1.getSaldo());
+
+        Notificacion[] notificaciones = new Notificacion[2];
+        notificaciones [0] = new NotificacionCorreo("Sebas");
+        notificaciones [1] = new NotificacionSMS("Juan");
+
+        notificaciones[0].enviarMensaje("Hola mi amigo");
+        notificaciones[1].enviarMensaje("Hola bro");
     }
 }
